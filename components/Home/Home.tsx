c90fd405d48f5e3d5048893f68433a853ef2a998
rajawatshivpratap46 <shiva18021912@gmail.com>
2026-04-08 00:40:40 +0530

    dark and light mode functionality is completed

---
+++ components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ThemeToggler from '../Helper/ThemeToggler';
+
+const Home = () => {
+  return (
+    <div className="overflow-hidden">
+        <ThemeToggler/> 
+    </div>
+  )
+};
+
+export default Home;
